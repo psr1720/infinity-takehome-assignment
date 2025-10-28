518e54f4068e6069453fb6212db7a264e6e97cf3
psr1720 <parasuram.reddy18@gmail.com>
2025-10-28 20:31:13 +0530

    Initial commit

---
+++ src/main/java/com/example/infinity_webhook_service/InfinityWebhookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.infinity_webhook_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinityWebhookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfinityWebhookServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/infinity_webhook_service/InfinityWebhookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infinity_webhook_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfinityWebhookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
